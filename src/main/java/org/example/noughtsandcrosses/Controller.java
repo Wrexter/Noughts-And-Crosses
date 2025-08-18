90a65e05861bd49472292f362a1db0e5bb33d6f6
Marcin Domżalski <domzalskim@gmail.com>
2025-08-18 20:51:11 +0200

    Win conditions

--- src/main/java/org/example/noughtsandcrosses/Controller.java
+++ src/main/java/org/example/noughtsandcrosses/Controller.java
@@ -2,6 +2,7 @@ package org.example.noughtsandcrosses;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 
 public class Controller {
@@ -42,6 +43,25 @@ public class Controller {
         }
         clicked.setDisable(true);
 
+        if (checkWin()) {
+            System.out.println("Wygrywa: " + clicked.getText());
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Koniec gry");
+            alert.setHeaderText(null);
+            alert.setContentText("Wygrywa: " + clicked.getText());
+            alert.showAndWait();
+            for (Button b : buttons) {
+                b.setDisable(true);
+            }
+        } else if (isBoardFull()) {
+            System.out.println("Remis!");
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Koniec gry");
+            alert.setHeaderText(null);
+            alert.setContentText("Remis!");
+            alert.showAndWait();
+        }
+
     }
 
     @FXML
@@ -58,5 +78,38 @@ public class Controller {
     private void handleExit(ActionEvent event) {
         System.out.println("Exit clicked");
         System.exit(0);
+    }
+
+    @FXML
+    private boolean checkWin() {
+        int[][] winPatterns = {
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8},
+                {0, 3, 6},
+                {1, 4, 7},
+                {2, 5, 8},
+                {0, 4, 8},
+                {2, 4, 6}
+        };
+        for (int[] pattern : winPatterns) {
+            String t1 = buttons[pattern[0]].getText();
+            String t2 = buttons[pattern[1]].getText();
+            String t3 = buttons[pattern[2]].getText();
+
+            if (!t1.isEmpty() && t1.equals(t2) && t2.equals(t3)) {
+                return true; // mamy zwycięzcę
+            }
+        }
+        return false;
+    }
+
+    private boolean isBoardFull() {
+        for (Button b : buttons) {
+            if (b.getText().isEmpty()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
