79d3d0eead75e4758918777016e474c361adb873
Marcin Domżalski <domzalskim@gmail.com>
2025-08-25 21:11:51 +0200

    Added icons and some visuals

--- src/main/java/org/example/noughtsandcrosses/Application.java
+++ src/main/java/org/example/noughtsandcrosses/Application.java
@@ -4,9 +4,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Optional;
 
 public class Application extends javafx.application.Application {
@@ -15,6 +17,7 @@ public class Application extends javafx.application.Application {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Noughts and crosses");
+        stage.getIcons().add(new Image(Objects.requireNonNull(Application.class.getResourceAsStream("/icons/NoughtsAndCrossesIcon.png"))));
         stage.setResizable(false);
         stage.setScene(scene);
 
@@ -28,6 +31,9 @@ public class Application extends javafx.application.Application {
         ButtonType pvp = new ButtonType("Player vs Player");
         ButtonType pvb = new ButtonType("Player vs Bot");
         setGameMode.getButtonTypes().setAll(pvp, pvb);
+
+        Stage alertStage = (Stage) setGameMode.getDialogPane().getScene().getWindow();
+        alertStage.getIcons().add(new Image(Objects.requireNonNull(Application.class.getResourceAsStream("/icons/NoughtsAndCrossesIcon.png"))));
 
         Optional<ButtonType> result = setGameMode.showAndWait();
         if (result.isPresent() && result.get() == pvp) {
--- src/main/java/org/example/noughtsandcrosses/Controller.java
+++ src/main/java/org/example/noughtsandcrosses/Controller.java
@@ -5,12 +5,16 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioMenuItem;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Random;
 
 public class Controller {
+    @FXML private Button btn0;
     @FXML private Button btn1;
     @FXML private Button btn2;
     @FXML private Button btn3;
@@ -19,7 +23,6 @@ public class Controller {
     @FXML private Button btn6;
     @FXML private Button btn7;
     @FXML private Button btn8;
-    @FXML private Button btn9;
 
     @FXML private RadioMenuItem mnPVP;
     @FXML private RadioMenuItem mnPVB;
@@ -51,7 +54,7 @@ public class Controller {
 
     @FXML
     private void initialize(){
-        buttons = new Button[]{btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9};
+        buttons = new Button[]{btn0, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8};
         for(Button b: buttons){
             b.setFocusTraversable(false);
         }
@@ -76,20 +79,20 @@ public class Controller {
         clicked.setDisable(true);
 
         if (checkWin()) {
-            System.out.println("Wygrywa: " + clicked.getText());
-            showAlert("Wygrywa: " + clicked.getText());
+            System.out.println('"' + clicked.getText() + '"' + " wins!");
+            showAlert('"' + clicked.getText() + '"' + " wins!");
             for (Button b : buttons) {
                 b.setDisable(true);
             }
         } else if (isBoardFull()) {
             System.out.println("Remis!");
-            showAlert("Remis");
+            showAlert("Draw!");
         }
 
     }
 
     @FXML
-    private void handleNewGame(ActionEvent event) {
+    private void handleNewGame() {
         System.out.println("New game clicked");
         clearBoard();
     }
@@ -97,6 +100,7 @@ public class Controller {
     private void clearBoard(){
         for(Button b: buttons){
             b.setText("");
+            b.setStyle("");
             b.setDisable(false);
         }
         currentPlayer = "O";
@@ -104,9 +108,21 @@ public class Controller {
     }
 
     @FXML
-    private void handleExit(ActionEvent event) {
+    private void handleExit() {
         System.out.println("Exit clicked");
         System.exit(0);
+    }
+
+    @FXML
+    private void handleAbout() {
+        Alert about = new Alert(Alert.AlertType.INFORMATION);
+        about.setTitle("About");
+        about.setHeaderText("Noughts and crosses\n" +
+                "Author: Marcin Domżalski");
+        about.setContentText("This is a simple noughts and crosses game with single player and player versus player modes");
+        Stage stage = (Stage) about.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(Objects.requireNonNull(Application.class.getResourceAsStream("/icons/NoughtsAndCrossesIcon.png"))));
+        about.show();
     }
 
     @FXML
@@ -133,6 +149,9 @@ public class Controller {
             String t3 = buttons[pattern[2]].getText();
 
             if (!t1.isEmpty() && t1.equals(t2) && t2.equals(t3)) {
+                buttons[pattern[0]].setStyle("-fx-text-fill: red;");
+                buttons[pattern[1]].setStyle("-fx-text-fill: red;");
+                buttons[pattern[2]].setStyle("-fx-text-fill: red;");
                 return true;
             }
         }
@@ -150,9 +169,11 @@ public class Controller {
 
     private void showAlert(String message){
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
-        alert.setTitle("Koniec gry");
+        alert.setTitle("Game Over!");
         alert.setHeaderText(null);
-        alert.setContentText(message);
+        alert.setContentText(message + "\nFor new game press F1");
+        Stage stage = (Stage) alert.getDialogPane().getScene().getWindow();
+        stage.getIcons().add(new Image(Objects.requireNonNull(Application.class.getResourceAsStream("/icons/NoughtsAndCrossesIcon.png"))));
         alert.showAndWait();
     }
 
@@ -161,12 +182,10 @@ public class Controller {
             for(int[] pattern: winPatterns){
                 int countX = 0;
                 int emptyIndex = -1;
-                int countO = 0;
 
                 for(int i = 0; i<3; i++){
                     String value = buttons[pattern[i]].getText();
                     if (value.equals("X")) countX++;
-                    else if (value.equals("O")) countO++;
                     else if (value.isEmpty()) emptyIndex = pattern[i];
                 }
 
@@ -177,18 +196,30 @@ public class Controller {
                     return;
                 }
 
-                else if (countO == 2 && emptyIndex != -1) {
+            }
+
+            for(int[] pattern: winPatterns){
+                int countO = 0;
+                int emptyIndex = -1;
+
+                for(int i = 0; i<3; i++){
+                    String value = buttons[pattern[i]].getText();
+                    if (value.equals("O")) countO++;
+                    else if (value.isEmpty()) emptyIndex = pattern[i];
+                }
+
+                if (countO == 2 && emptyIndex != -1) {
                     buttons[emptyIndex].setText("X");
                     buttons[emptyIndex].setDisable(true);
                     currentPlayer = "O";
                     return;
                 }
+
             }
 
-
-            if(btn5.getText().isEmpty()){
-                btn5.setText("X");
-                btn5.setDisable(true);
+            if(btn4.getText().isEmpty()){
+                btn4.setText("X");
+                btn4.setDisable(true);
                 currentPlayer = "O";
                 return;
             }
@@ -205,39 +236,6 @@ public class Controller {
                 buttons[chosenIndex].setDisable(true);
                 currentPlayer = "O";
             }
-
-            /*
-            if(btn5.getText().isEmpty()){
-                btn5.setText("X");
-                btn5.setDisable(true);
-                currentPlayer = "O";
-                return;
-            }
-
-            int[] corners = {0, 2, 6, 8};
-            for (int idx : corners) {
-                if (buttons[idx].getText().isEmpty()) {
-                    buttons[idx].setText("X");
-                    buttons[idx].setDisable(true);
-                    currentPlayer = "O";
-                    return;
-                }
-            }
-
-            int[] sides = {1, 3, 5, 7};
-            for (int idx : sides) {
-                if (buttons[idx].getText().isEmpty()) {
-                    buttons[idx].setText("X");
-                    buttons[idx].setDisable(true);
-                    currentPlayer = "O";
-                    return;
-                }
-            }
-             */
-
-
-
         }
-
     }
 }
